6d5e27228fac02dc96f60666be28f9dbee65821f
Elizabeth Le-luan <39098891+E-LeLuan@users.noreply.github.com>
2019-06-12 12:39:38 +0100

    Create install.R

---
+++ install.R
@@ -0,0 +1,5 @@
+install.packages("ggplot2")
+install.packages("dplyr")
+
+#This is not nessecary for the script example as it only requires base R.
+#However, this is how you would add any packages your script requires.
